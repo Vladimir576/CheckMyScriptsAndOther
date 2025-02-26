4b5d6c1b70dba91f9c95ab516a62be29bd3f1bda
Goldobin Vladimir <arue11@mail.ru>
2025-02-26 19:02:31 +0600

    Link update

--- scripts/pageScroll.js
+++ scripts/pageScroll.js
@@ -1,80 +1,3 @@
-// const menuItems = document.querySelectorAll('.fixed-menu__item');
-// let itemsLength = menuItems.length * 100;
-// let wrapperVH = document.getElementById('wrapper');
-// let currentTranslateY = 0;
-// let isAnimating = false;
-
-
-
-// function updateActiveMenuItem(index) {
-//     menuItems.forEach((item, i) => {
-//         if (i === index) {
-//             item.classList.add('fixed-menu__item--active');
-//         } else { 
-//             item.classList.remove('fixed-menu__item--active');
-//         }
-//     });
-// }
-
-
-// menuItems.forEach((item, index) => {
-//     item.addEventListener('click', (e) => {
-//         e.preventDefault();
-//         if (isAnimating) return;
-
-//         currentTranslateY = -index * 100;
-//         wrapperVH.style.transform = `translateY(${currentTranslateY}vh)`;
-
-//         updateActiveMenuItem(index);
-
-//         isAnimating = true;
-
-//         setTimeout(() => {
-//             isAnimating = false;
-//         }, 1000);
-//     });
-// });
-
-// document.addEventListener('wheel', function (event) {
-//     if (isAnimating) {
-//         event.preventDefault();
-//         return;
-//     }
-
-//     if (event.deltaY > 0 && currentTranslateY > -itemsLength + 100) {
-//         currentTranslateY -= 100;
-//         wrapperVH.style.transform = `translateY(${currentTranslateY}vh)`;
-//         isAnimating = true;
-
-//         setTimeout(() => {
-//             isAnimating = false;
-//         }, 1000);
-//     }
-
-//     else if (event.deltaY < 0 && currentTranslateY < 0) {
-//         currentTranslateY += 100;
-//         wrapperVH.style.transform = `translateY(${currentTranslateY}vh)`;
-//         isAnimating = true;
-
-//         setTimeout(() => {
-//             isAnimating = false;
-//         }, 1000);
-//     }
-
-//     let currentIndex = Math.abs(currentTranslateY / 100);
-//     updateActiveMenuItem(currentIndex);
-
-
-// });
-
-
-
-
-
-
-
-
-
 const menuItems = document.querySelectorAll('.fixed-menu__item');
 const anchorLinks = document.querySelectorAll('.nav__link');
 var modalLinks = document.querySelectorAll('.modal-item__link');
@@ -165,7 +88,7 @@ document.addEventListener('wheel', function (event) {
 
 
 // Модальное меню при 768px  
-// 
+
 modalLinks.forEach((link, index) => {
     link.addEventListener('click', (e) => {
         e.preventDefault();
